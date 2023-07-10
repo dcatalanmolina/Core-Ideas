94819b0b60ede12271a9f9702a60fb0666afe5fc
Diego Catalan <diego@helloinsight.org>
2023-07-10 09:33:37 -0700

    Added new vars created with simple operations

--- code/clean_data_examples.R
+++ code/clean_data_examples.R
@@ -16,5 +16,17 @@ core_data <- read_csv("data/core_data.csv")
 clean_data <- 
   core_data %>%
   mutate( # create new variables
+    sb_scaled = sb*100,
+    sb_ach = sb + ach,
+    sb_divided = sb/2,
+    sb_minus = sb - threat
 
   )
+
+# check your results
+head(clean_data$sb_scaled)
+head(clean_data$sb)
+head(clean_data$threat)
+head(clean_data$sb_ach)
+head(clean_data$sb_divided)
+head(clean_data$sb_minus)
