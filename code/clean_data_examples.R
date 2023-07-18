08f09643f4408de008ed155cb1f01c224d0beaa2
Diego Catalan <diego@helloinsight.org>
2023-07-18 06:33:06 -0700

    Adds code to create variables based on conditions

--- code/clean_data_examples.R
+++ code/clean_data_examples.R
@@ -16,17 +16,61 @@ core_data <- read_csv("data/core_data.csv")
 clean_data <- 
   core_data %>%
   mutate( # create new variables
+    # basic operations
     sb_scaled = sb*100,
     sb_ach = sb + ach,
     sb_divided = sb/2,
-    sb_minus = sb - threat
+    sb_minus = sb - threat,
+    
+    # based on conditions
+    threat_over_zero = 
+      if_else(
+        threat > 0, # condition
+        1, # value if true
+        0, # value if false
+      ),
+    
+    threat_ach_over_zero = 
+      if_else(
+        threat > 0 | ach > 0, # condition
+        1, # value if true
+        0, # value if false
+      ),
+    
+    ach_high = 
+      if_else(
+        ach > mean(ach, na.rm = TRUE),
+        "high",
+        "low"
+      ),
+    
+    # use case_when to use multiple T or F
+    # statements
+    cases = 
+      case_when(
+        ach_high == "low" ~ "case1",
+        ach_high == "high" & threat > 0 ~ "case2",
+        TRUE ~ "case3"
+      ),
+    
+    # use str_detect when you want to match
+    # parts of a word or string to each row
+    cases_2 = 
+      case_when(
+        str_detect(ach_high, "lo") ~ "case1",
+        str_detect(ach_high, "hi") & threat > 0 ~ "case2",
+        TRUE ~ "case3"
+      )
+    
 
   )
 
 # check your results
-head(clean_data$sb_scaled)
-head(clean_data$sb)
-head(clean_data$threat)
-head(clean_data$sb_ach)
-head(clean_data$sb_divided)
-head(clean_data$sb_minus)
+clean_data %>% 
+  select(ach, ach_high) %>% 
+  head(10)
+
+mean(clean_data$ach, na.rm = TRUE)
+
+table(clean_data$cases)
+table(clean_data$cases_2)
